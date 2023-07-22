5fbef36f20282789d60aa8e101a595311d92c805
Radu-Alexandru Bulai <radualexandrubulai@gmail.com>
2023-07-22 09:54:22 +0300

    Add Angular PingServerByItsIpAddress method with spinning loading icon `app.component.ts` - `pingServerByItsIpAddress()`: - `this.ipAddressSubjectWhenPinging.next(ipAddress);` from the HTML code, we added a condition that whenever this "variable" (that has its observer on ` readonly ipAddressStatusWhenPinging$ = this.ipAddressSubjectWhenPinging.asObservable();`) is populated, we will show a loading spinner in UI (if this `ipAddressSubjectWhenPinging` is empty then we will show the "Ping" button in UI) - `this.serverService.pingServerByIpAddress$` will make the ping request to the REST SpringBoot API (by calling `pingServer` method from SpringBoot) which will return a **CustomResponse** containing the updated server (with its status updated). - we will keep a copy of all the Servers that are already rendered/retrieved in the UI, this copy will be inside `serversCopyDataSubject = new BehaviorSubject<CustomResponse>(null!);` (and the servers data will be inside the value property, such as `const serversCopy = this.serversCopyDataSubject.value.data.servers;`) - once we retrieve the response from backend server:     - now we need to update our Server from the Servers list from UI (stored as a copy)     - we will identify the server (that has been pinged) from our copy of servers list in UI by founding its index     - to find the equivalent server in our UI, we will search the list by comparing the IDs of any server from our UI list vs the id of updated server retrieved as response from backend     - note that this `pingServerByItsIpAddress` called from `app.component.html` will return (in `this.appState$`) the entire copy of servers list from UI that contains the updated server, therefore the UI will be updated     - `this.ipAddressSubjectWhenPinging.next('');` assign an empty string to show back the "Ping" button in UI (`app.component.html`) instead of spinning loading icon (Radu-Alexandru Bulai - Saturday, July 22, 2023, 09:54)

--- serverpingstatustracker-app/src/app/app.component.ts
+++ serverpingstatustracker-app/src/app/app.component.ts
@@ -13,6 +13,11 @@ import { CustomResponse } from './interfaces/custom-response';
 import { DataState } from './enums/data-state.enum';
 import { Status } from './enums/status.enum';
 
+/**
+ * @author Radu-Alexandru Bulai
+ * @version 1.0.0
+ * @since 18/07/2023
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,19 +27,53 @@ export class AppComponent implements OnInit {
   appState$!: Observable<AppState<CustomResponse>>;
   readonly Status = Status;
   readonly DataState = DataState;
-  private filterSubjectWhenPinging = new BehaviorSubject<string>('');
-  readonly filterStatusWhenPinging$ =
-    this.filterSubjectWhenPinging.asObservable();
+  private ipAddressSubjectWhenPinging = new BehaviorSubject<string>('');
+  readonly ipAddressStatusWhenPinging$ =
+    this.ipAddressSubjectWhenPinging.asObservable();
+  private serversCopyDataSubject = new BehaviorSubject<CustomResponse>(null!);
+  // Assert that null can be assigned by using the non-null assertion operator !
+  // This assumes that the data passed to serversDataCopySubject (the UI copy of servers)
+  // will be updated before any subscribers access it, ensuring that it won't actually be null.
 
   constructor(private serverService: ServerService) {}
 
   ngOnInit(): void {
     this.appState$ = this.serverService.servers$.pipe(
       map((response) => {
+        this.serversCopyDataSubject.next(response);
         return { dataState: DataState.LOADED_STATE, appData: response };
       }),
       startWith({ dataState: DataState.LOADING_STATE }),
       catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error });
+      })
+    );
+  }
+
+  pingServerByItsIpAddress(ipAddress: string): void {
+    // Assign ip string to show a spinning loading icon while pinging
+    this.ipAddressSubjectWhenPinging.next(ipAddress);
+    this.appState$ = this.serverService.pingServerByIpAddress$(ipAddress).pipe(
+      map((response) => {
+        const serversCopy = this.serversCopyDataSubject.value.data.servers;
+        const indexOfPingedServer = serversCopy!.findIndex(
+          (server) => server.id === response.data.server!.id
+        );
+        // Update the Server from serversCopy in UI after it has been pinged
+        serversCopy![indexOfPingedServer] = response.data.server!;
+        // Assign empty string to stop showing spinning loading icon
+        this.ipAddressSubjectWhenPinging.next('');
+        return {
+          dataState: DataState.LOADED_STATE,
+          appData: this.serversCopyDataSubject.value,
+        };
+      }),
+      startWith({
+        dataState: DataState.LOADED_STATE,
+        appData: this.serversCopyDataSubject.value,
+      }),
+      catchError((error: string) => {
+        this.ipAddressSubjectWhenPinging.next('');
         return of({ dataState: DataState.ERROR_STATE, error });
       })
     );
