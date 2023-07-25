6b415f12501aedaa8a4e7ae15ba7ba960088d9b4
Radu-Alexandru Bulai <radualexandrubulai@gmail.com>
2023-07-26 00:01:51 +0300

    Create Angular Footer Component

--- serverpingstatustracker-app/src/app/app.module.ts
+++ serverpingstatustracker-app/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
