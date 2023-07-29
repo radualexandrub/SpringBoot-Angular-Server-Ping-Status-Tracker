0703387f1a7a03b82bc5ade57132bd16732c20a2
Radu-Alexandru Bulai <radualexandrubulai@gmail.com>
2023-07-29 14:25:53 +0300

    Create About Page Route in Angular App 1\) Create About page by running `ng generate component components/pages/aboutPage` 2\) In `app.module.ts` - import "Routes", define "appRoutes" where routes are specified - add `imports: [RouterModule.forRoot(appRoutes, { enableTracing: false })]` (this should be set on true for debugging purposes and false in production) 3\) In `app.component.html`, instead of `<app-servers>` component we add `<router-outlet>` 4\) Add contents to `about-page.component.html` 5\) In `header.component.ts` - make sure "private router: Router" is passed in contructor - make sure "hasRoute" and "hasRouteIncluded" methods are defined 6\) In `header.component.html` - add `*ngIf="hasRoute('/')"` on "Add Server" button to hide it from navbar when we are in "About" page - add `*ngIf="hasRoute('/about')"` to show Servers(/) link when in "About" page - add `[ngClass]="{ active: hasRoute('/about') }"` to show "About" link page as active
    (Saturday, July 29, 2023, 14:27 - Radu-Alexandru Bulai)
    Reuse of personal notes from https://github.com/radualexandrub/Study/blob/master/Angular/README.md#angular-router and commit from previous EmployeeManager application https://github.com/radualexandrub/SpringBoot-Angular-EmployeeManagerApp/commit/572dbe053a617cd1f11730a9daee5444494adb67#diff-5468d09e26643f32f6d6f60deb0960d87f4f5bd8e6da893a27453e3f9ccd9116

--- serverpingstatustracker-app/src/app/app.module.ts
+++ serverpingstatustracker-app/src/app/app.module.ts
@@ -2,21 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ServersComponent } from './components/servers/servers.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutPageComponent } from './components/pages/about-page/about-page.component';
 
+const appRoutes: Routes = [
+  { path: '', component: ServersComponent },
+  { path: 'about', component: AboutPageComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     ServersComponent,
     HeaderComponent,
+    AboutPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: false }),
+  ],
   providers: [ServersComponent],
   bootstrap: [AppComponent],
 })
